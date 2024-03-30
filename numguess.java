646eb73d8bbbfc06f88dffa37a298a83535e1b14
ARSHAK MUHAMMED P K <quantumexploreram@yahoo.com>
2024-03-30 20:36:24 +0530

    Update numguess.java
    added gui

--- numguess.java
+++ numguess.java
@@ -1,40 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
-import java.util.Scanner;
-public class numguess{
+
+public class numguess extends JFrame implements ActionListener {
+    private final JLabel promptLabel;
+    private final JTextField guessField;
+    private final JButton guessButton;
+    private final JTextArea outputArea;
+    private int num;
+    private int numberOfTurns;
+
+    public numguess() {
+        super("Number Guessing Game");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new FlowLayout());
+
+        Random rand = new Random();
+        num = rand.nextInt(100) + 1;
+        numberOfTurns = 8;
+
+        promptLabel = new JLabel("Guess a number between 1 and 100:");
+        guessField = new JTextField(10);
+        guessButton = new JButton("Guess!");
+        outputArea = new JTextArea(10, 30);
+        outputArea.setEditable(false);
+
+        guessButton.addActionListener(this);
+
+        add(promptLabel);
+        add(guessField);
+        add(guessButton);
+        add(outputArea);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == guessButton) {
+            int guess = Integer.parseInt(guessField.getText());
+            if (guess < 1 || guess > 100) {
+                outputArea.append("\nPlease enter a number between 1 and 100");
+                guessField.setText("");
+            }
+
+            else if (guess == num) {
+                outputArea.setText("Congrats! You got it in " + (8-numberOfTurns) + " guesses");
+                guessButton.setEnabled(false);
+            } else if (guess < num) {
+                outputArea.append("\nYour guess is too low");
+                numberOfTurns--;
+                outputArea.append("\nYou have " + numberOfTurns + " guesses left");
+            } else if (guess > num) {
+                outputArea.append("\nYour guess is too high");
+                numberOfTurns--;
+                outputArea.append("\nYou have " + numberOfTurns + " guesses left");
+            }
+
+            if (numberOfTurns == 0 && guess != num) {
+                outputArea.append("\n\nYou ran out of guesses... Better Luck Next Time!!!");
+                outputArea.append("\nThe Number Is " + num);
+                guessButton.setEnabled(false);
+            }
+
+            guessField.setText("");
+        }
+    }
+
     public static void main(String[] args) {
-        Random rand = new Random();	
-        int num = rand.nextInt(100) + 1;
-        int number_of_turns = 8;
-	
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Guess a number between 1 and 100");
-        int guess = sc.nextInt();
-
-        while(number_of_turns!=1){
-            if (guess == num){
-                System.out.println("Congrats! You got it in " + number_of_turns + " guesses");
-                System.exit(0);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                numguess game = new numguess();
+                game.setVisible(true);
             }
-            else if (guess < num){
-                System.out.println("Your guess is too low");
-                number_of_turns--;
-                System.out.println("You have " + number_of_turns + " guesses left");
-                System.out.println("Enter your guess:");
-                guess = sc.nextInt();
-            }
-            else if (guess > num){
-                System.out.println("Your guess is too high");
-                number_of_turns--;
-                System.out.println("You have " + number_of_turns + " guesses left");
-                System.out.println("Enter your guess:");
-                guess = sc.nextInt();
-            }
-        }
-            System.out.println("You ran out of guesses ..Better Luck Next Time !!!");
-            System.out.println("The Number Is " + num);
-            System.exit(0);
-        }
-        
-
-
+        });
+    }
 }
